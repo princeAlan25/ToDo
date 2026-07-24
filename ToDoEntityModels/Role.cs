6517d0870fec274ac7b328a432ecdb269483d458
princeAlan25 <prince.alain.loui@gmail.com>
2026-07-24 20:20:49 +0200

    Add project files.

---
+++ ToDoEntityModels/Role.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ToDoEntityModels
+{
+    public class Role
+    {
+        [Required]
+        [Key]
+        public int RoleId { get; set; }
+        [Required]
+        [StringLength(30)]
+        public string Name { get; set; } = "guest";
+        public Guid UserId { get; set; }
+    }
+}
---
+++ ToDoEntityModels/User.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace ToDoEntityModels;
+
+public class User
+{
+    [Required]
+    [Key]
+    public Guid UserId { get; set; } = Guid.NewGuid();
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; } = "";
+    [AllowNull]
+    public string UserName { get; set; } = "";
+    [Required]
+    public string Password { get; set; } = "";
+}
