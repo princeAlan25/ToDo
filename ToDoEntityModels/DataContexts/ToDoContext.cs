da4891611508655e31b2f3b9d9eee80deb619a87
princeAlan25 <prince.alain.loui@gmail.com>
2026-07-25 17:09:58 +0200

    Add ToDoContextExtension for DI and SQLite config
    Introduce ToDoContextExtension with AddToDoContextService to register ToDoContext using SQLite in a "Database" directory and enable EF Core command logging. Add required using directives for DI and EF Core.

--- ToDoEntityModels/DataContexts/ToDoContext.cs
+++ ToDoEntityModels/DataContexts/ToDoContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using ToDoEntityModels.Models;
 
 namespace ToDoEntityModels.DataContexts;
---
+++ ToDoEntityModels/DataContexts/ToDoContextExtension.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToDoEntityModels.DataContexts;
+
+public static class ToDoContextExtension
+{
+    public static IServiceCollection AddToDoContextService(
+        this IServiceCollection services,
+        string databaseFile = "todo.db"
+        )
+    {
+        string dbFile = databaseFile;
+        string dbPath = Path.GetFullPath(Path.Combine(Directory.GetCurrentDirectory(), "Database"));
+        string fullDbPath = Path.GetFullPath(Path.Combine(dbPath, dbFile));
+        services.AddDbContext<ToDoContext>(options =>
+        {
+            options.UseSqlite($"Data Source={fullDbPath}");
+
+            options.LogTo(
+                ContextLogger.LoggContext,
+                [Microsoft.EntityFrameworkCore.Diagnostics.RelationalEventId.CommandExecuting]
+                );
+        }, contextLifetime: ServiceLifetime.Transient, optionsLifetime: ServiceLifetime.Transient);
+        return services;
+    }
+}
