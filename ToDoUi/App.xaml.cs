3f9e74227a05745519b62ddb104ebb2ddbe80ab7
princeAlan25 <prince.alain.loui@gmail.com>
2026-08-24 20:29:15 +0200

    Add Material You palette & theme system to MAUI app
    Introduced CustomPalette.xaml with Material Design 3 color tokens and interactive states for light/dark themes. Updated Colors.xaml, App.xaml, and App.xaml.cs to load and apply new resources, including dynamic title bar theming. Added CustomStyles.xaml with new Button/ImageButton styles and visual states. Updated Styles.xaml and AppShell.xaml to use new color resources and styles. Included new app title icon in ToDoUi.csproj.

--- ToDoUi/App.xaml.cs
+++ ToDoUi/App.xaml.cs
@@ -6,15 +6,50 @@ namespace ToDoUi;
 
 public partial class App : Application
 {
+    private Color _bgThemeMode = Color.FromArgb("#FFFBF8FF");
+    private Color _fgThemeMode = Color.FromArgb("#FF1A1B21");
+    private readonly string _icon = "apptitleicon.png";
+    private Window _window;
     private readonly AppShell _shell;
     public App(AppShell shell)
     {
         InitializeComponent();
         _shell = shell;
+        _window = new Window(_shell);
+        Application.Current?.RequestedThemeChanged += App_RequestedThemeChanged;
     }
 
     protected override Window CreateWindow(IActivationState? activationState)
     {
-        return new Window(_shell);
+        //initial theme binding
+        _window.TitleBar = new TitleBar
+        {
+            Title = "Todo Sphere",
+            Icon = _icon,
+            BackgroundColor = Application.Current?.RequestedTheme == AppTheme.Light
+            ? Color.FromArgb("#FFFBF8FF")
+            : Color.FromArgb("#FF1A1B21"),
+            ForegroundColor = Application.Current?.RequestedTheme == AppTheme.Light
+            ? Color.FromArgb("#FF1A1B21")
+            : Color.FromArgb("#FFE3E1E9")
+        };
+        return _window;
+    }
+
+    private void App_RequestedThemeChanged(object? sender, AppThemeChangedEventArgs e)
+    {
+        _bgThemeMode = Application.Current?.RequestedTheme == AppTheme.Light
+            ? Color.FromArgb("#FFFBF8FF")
+            : Color.FromArgb("#FF1A1B21");
+        _fgThemeMode = Application.Current?.RequestedTheme == AppTheme.Light
+            ? Color.FromArgb("#FF1A1B21")
+            : Color.FromArgb("#FFE3E1E9");
+        _window.TitleBar = new TitleBar
+        {
+            Title = "Todo Sphere",
+            Icon = _icon,
+            BackgroundColor = _bgThemeMode,
+            ForegroundColor = _fgThemeMode
+        };
     }
 }
